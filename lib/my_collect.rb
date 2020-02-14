a0dac57e447a9f5460617fee7deed3238f914185
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-14 20:15:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,15 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(array)
   i = 0
   result = []
   while i < array.length
-    result.push(yield array[i])
+    result.push(yield array[i]) #yield is like calling another method
       i += 1
   end
   result
 end
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#yield goes here
 my_collect(array) do |name|
   name.split(" ").first
 end
